f920573c7a2bde73f0a244f40267305571d00359
JeniaGurman <144277083+JeniaGurman@users.noreply.github.com>
2024-01-29 14:30:17 +0200

    lesson 3

--- js/Lesson-1-3.js
+++ js/Lesson-1-3.js
@@ -399,9 +399,41 @@ function calculateTax(amount, taxRate) {
 }
 const aaa = calculateTax(200, 0.2)
 console.log(aaa);
+/////////////////////////////////
+function slugify(title) {
+        return title.split(` `).join(`-`).toLowerCase();
+}
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+///////////////////////////////////////////////////////
+function makeArray(firstArray, secondArray, maxLength) {
+    if (firstArray.concat(secondArray).length >= maxLength) 
+    {return firstArray.concat(secondArray).slice(0, maxLength)}
+}
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+//////////////////////////////////////////////////////////
+function filterArray(numbers, value) {
+    let total = []
+    for ( let number of numbers) {
+        if (value  < number) { total.push(number)
 
-
-
+        }
+    }
+    return total;
+    
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
 
 
 
